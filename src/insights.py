825d5381ff77f0234807c882c8346ae897b3a012
Vitor Campos <vitorcampos742@gmail.com>
2022-09-13 17:02:49 -0300

    requisito 7

--- src/insights.py
+++ src/insights.py
@@ -38,21 +38,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    dataList = []
+    for job in jobs:
+        if job['industry'] == industry:
+            dataList.append(job)
+    return dataList
 
 
 def get_max_salary(path):
