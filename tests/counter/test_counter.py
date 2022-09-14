553420e804f5b04bffa86a0a595264945cd7dcf7
Vitor Campos <vitorcampos742@gmail.com>
2022-09-13 21:39:35 -0300

    tests

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -4,5 +4,6 @@ from src.counter import count_ocurrences
 def test_counter():
     'a funcao count_ocurrences deve funcionar corretamente'
     count = count_ocurrences('src/jobs.csv', 'Python')
-    print(type(count))
-    assert count is 1639
+    assert count == 1639
+    count = count_ocurrences('src/jobs.csv', 'Javascript')
+    assert count != 10
