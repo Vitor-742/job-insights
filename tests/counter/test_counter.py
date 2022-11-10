2ca947e62ff815d7d6cde348a399e3d955144f1d
Vitor Campos <vitorcampos742@gmail.com>
2022-11-09 21:20:19 -0300

    alteracao readme

--- tests/counter/test_counter.py
+++
@@ -1,9 +0,0 @@
-from src.counter import count_ocurrences
-
-
-def test_counter():
-    'a funcao count_ocurrences deve funcionar corretamente'
-    count = count_ocurrences('src/jobs.csv', 'Python')
-    assert count == 1639
-    count = count_ocurrences('src/jobs.csv', 'Javascript')
-    assert count != 10
